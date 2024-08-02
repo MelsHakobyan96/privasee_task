d0de087101bc872ce7fd0d561916562f7723600b
Mels <40002880+MelsHakobyan96@users.noreply.github.com>
2024-08-02 18:14:24 +0400

    Update config.py

--- privasee/config.py
+++ privasee/config.py
@@ -1,3 +1,3 @@
-openai_api_key = "sk-proj-oadDz5mYucNRx0N3KtSWT3BlbkFJ7X3tiXqxrkqZsenmc5i6"
-openai_organization = "org-M46elCVaTjopTSA83MNOIgPc"
+openai_api_key = ""
+openai_organization = ""
 gpt_model = "gpt-4-turbo" # You can change the model as well
