0552166175554962936a1648b8963f91ceac9a06
MelsHakobyan96 <mels.hakobyan96@gmail.com>
2024-09-05 21:37:00 +0400

    added Feedback Intelligence

--- privasee/config.py
+++ privasee/config.py
@@ -1,3 +1,4 @@
-openai_api_key = "sk-proj-oadDz5mYucNRx0N3KtSWT3BlbkFJ7X3tiXqxrkqZsenmc5i6"
-openai_organization = "org-M46elCVaTjopTSA83MNOIgPc"
-gpt_model = "gpt-4-turbo" # You can change the model as well
+openai_api_key = ""
+openai_organization = ""
+gpt_model = ""
+fi_api_key = ""
